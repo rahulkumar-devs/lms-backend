3a6e6447bcef991afac189bfdcff26bfa351f8f0
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-04 04:39:21 +0530

    add reids connections with error handling

--- src/configurations/redis-connections.ts
+++ src/configurations/redis-connections.ts
@@ -1,19 +1,55 @@
-import Redis from "ioredis"
+import Redis from "ioredis";
 import config from "./config";
+import createHttpError from "http-errors";
 
-
-
-
-const redisClient = () => {
-    if (config.redis_uri) {
-        console.log({ status: "redis Connected" })
-        return config.redis_uri;
+const createRedisClient = () => {
+    if (!config.redis_uri) {
+        console.error("Redis connection failed: Missing URI");
+        throw createHttpError(400, 'Redis Failed to connect: Missing URI');
     }
-  
-    console.error("redis connection failed");
-    process.exit(1);
 
-}
+    return new Redis(config.redis_uri);
+};
 
-const redis = new Redis(redisClient());
+const redis = createRedisClient();
+
+redis.on("error", (err: any) => {
+    if (err.code === "ETIMEDOUT") {
+        console.error('Redis connection timed out:', err);
+        throw createHttpError(400, 'Redis connection timed out:', err);
+    } else if (err.code === "ENOTFOUND") {
+        console.error('Redis hostname not found:', err.hostname);
+        throw createHttpError(400, `Redis hostname not found: ${err.hostname}`, err);
+    } else {
+        console.error('Redis error:', err);
+        throw createHttpError(400, 'Redis Error', err);
+    }
+});
+
+redis.on('connect', () => {
+    console.log('Connected to Redis');
+});
+
+redis.on('ready', () => {
+    console.log('Redis connection is ready to use');
+});
+
+redis.on('end', () => {
+    console.log('Redis connection has ended');
+});
+
+process.on('SIGINT', () => {
+    redis.quit().then(() => {
+        console.log('Redis connection closed through app termination');
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', () => {
+    redis.quit().then(() => {
+        console.log('Redis connection closed through app termination');
+        process.exit(0);
+    });
+});
+
 export default redis;
