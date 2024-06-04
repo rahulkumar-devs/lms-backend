302c9819c5219196b2e428f50c92b9009d522de1
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-04 10:16:14 +0530

    modify errorHandler

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,38 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 
-export default (err: any, req: Request, res: Response, next: NextFunction) => {
-
+const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal server error";
+    err.message = err.message || "Internal Server Error";
 
+    // Handle Mongoose cast errors (invalid ObjectId)
     if (err.name === "CastError") {
-        const message = `Resource not found ${err.path}`;
+        const message = `Resource not found: ${err.path}`;
         err = createHttpError(400, message);
     }
 
-    //  Duplicate key error
+    // Handle Mongoose duplicate key errors
     if (err.code === 11000) {
-        const message = `Duplicate ${Object.keys(err.keyValue)} enterd`;
-        err = createHttpError(400, message);
-
-    }
-
-    if (err.name === "jsonWebToken") {
-        const message = `Invalid JsonWebToken`;
+        const message = `Duplicate field value entered: ${Object.keys(err.keyValue).join(", ")}`;
         err = createHttpError(400, message);
     }
 
+    // Handle JWT errors
+    if (err.name === "JsonWebTokenError") {
+        const message = `Invalid JSON Web Token`;
+        err = createHttpError(400, message);
+    }
+
+    // Handle JWT expired errors
     if (err.name === "TokenExpiredError") {
-        const message = ` JsonWebToken expired`;
+        const message = `JSON Web Token has expired`;
         err = createHttpError(400, message);
     }
 
+    // Send the error response
     res.status(err.statusCode).json({
         success: false,
-        message: err.message
-    })
+        message: err.message,
+    });
+};
 
-
-
-}
+export default errorHandler;
