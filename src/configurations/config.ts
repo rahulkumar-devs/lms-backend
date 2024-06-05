114e8f6e12c470550fb5a9f1ab18ecdbe222bedc
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-06 03:42:19 +0530

    chage some variable name

--- src/configurations/config.ts
+++ src/configurations/config.ts
@@ -3,12 +3,12 @@ import "dotenv/config";
 // _config make it private
 const _config = {
     port: process.env.PORT || 401,
-    client_url: process.env.CLIENT_URL || "",
     activate_token_key: process.env.ACTIVATE_TOKEN_KEY as string || "",
     origin_url:process.env.ORIGIN_URL ||"",
 
+
     mongoDB_uri: process.env.MONGODB_URI as string || "",
-    env: process.env.NODE_ENV || "",
+    node_env: process.env.NODE_ENV || "",
     redis_uri: process.env.REDIS_URI as string || "",
     cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
     cloudinary_api_key: process.env.CLOUDINARY_API_KEY || "",
@@ -18,13 +18,14 @@ const _config = {
     //    tokens details
     access_token_key: process.env.REFRESH_TOKEN_KEY as string,
     refresh_token_key: process.env.ACCESS_TOKEN_KEY as string,
-    access_token_expiry: process.env.REFRESH_TOKEN_EXPIRY as string,
-    refresh_token_expiry: process.env.ACCESS_TOKEN_EXPIRY as string,
+    access_token_expiry: parseInt(process.env.REFRESH_TOKEN_EXPIRY || '300',10) ,
+    refresh_token_expiry: parseInt(process.env.ACCESS_TOKEN_EXPIRY || '1200',10),
 
     // smtp server config
     smtp_user: process.env.SMTP_USER as string,
     smtp_host: process.env.SMTP_HOST as string,
     smtp_password: process.env.SMTP_PASSWORD as string,
+    smtp_service :process.env.SMTP_SERVICE,
 
     // Admin Emain
     //    admin_email:process.env.ADMIN_EMAIL
