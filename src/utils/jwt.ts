2a938c7305e5a2ffc157b0b9f21e0bf75a6c8456
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-06 03:39:41 +0530

    make changes

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,61 +1,60 @@
+
+
 import { Response } from "express";
 import { IUserSchema } from "../types/userTypes";
 import config from "../configurations/config";
-import sendResponse from "./sendResponse";
 import redis from "../configurations/redis-connections";
 
-
-interface ITokenOptions {
-    expires: Date;
+export interface ITokenOptions {
+    expires?: any;
     maxAge: number;
     httpOnly: boolean;
     sameSite: 'lax' | 'strict' | 'none' | undefined;
     secure?: boolean;
 }
 
+
+// Cookie options
+export const accessTokenOptions: ITokenOptions = {
+    expires: new Date(Date.now() + config.access_token_expiry ** 60 * 60 * 1000),
+    maxAge: config.access_token_expiry * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'none',
+    secure: config.node_env === "production"
+};
+
+
+
+export const refreshTokenOptions: ITokenOptions = {
+    expires: new Date(Date.now() + config.refresh_token_expiry * 24 * 60 * 60 * 1000), // Calculate expiry time
+    maxAge: config.refresh_token_expiry * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'none',
+    secure: config.node_env === "production"
+};
+
 const sendToken = async (
     user: IUserSchema,
     res: Response
 ) => {
     try {
-
         const accessToken = await user.signAccessToken();
         const refreshToken = await user.signRefreshToken();
 
 
-        redis.set(user._id as string, JSON.stringify(user));
+        // Store user data in Redis
+        await redis.set(user._id as string, JSON.stringify(user) as any);
 
-
-
-        const accessTokenOptions: ITokenOptions = {
-            expires: new Date(Date.now() + config.access_token_expiry * 1000),
-            maxAge: config.access_token_expiry * 1000,
-            httpOnly: true,
-            sameSite: 'lax'
-        }
-        const refreshTokenOptions: ITokenOptions = {
-            expires: new Date(Date.now() + config.refresh_token_expiry * 1000),
-            maxAge: config.refresh_token_expiry * 1000,
-            httpOnly: true,
-            sameSite: 'lax'
-        }
-
-        if (config.node_env === "production") {
-            accessTokenOptions.secure = true;
-        }
-
-        res.cookie("access-token", accessToken, accessTokenOptions).cookie("refresh-token", refreshToken, refreshTokenOptions).json({
+        // Set cookies
+        res.cookie("accessToken", accessToken, accessTokenOptions).cookie("refreshToken", refreshToken, refreshTokenOptions).status(200).json({
             success: true,
             message: "Token attached",
             user,
             accessToken
-        })
-
-
-
+        });
     } catch (error: any) {
-        throw new Error(error)
+        throw new Error(error);
     }
-}
+};
 
-export default sendToken
+export default sendToken;
