7739ed98812237a6ba9c23cfb9f77f2b199c1dff
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-06 09:02:21 +0530

    add some service routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,11 +2,38 @@
 import express from "express";
 import registrationsUser from "../controllers/auth/registerUser";
 import activateAccount from "../controllers/auth/activateAccount";
+import { loginUser, logoutUser } from "../controllers/auth/loginUser";
+import { isAuthenticated } from "../middleware/authentication";
+import authorizedRole from "../middleware/authorizedRole";
+import { updateAccessToken } from "../controllers/auth/updateAccessToken";
+import { changeEmail, emailVerification, getUserById, updateUserInfo } from "../services/user.service";
 
 const userRouter = express.Router();
 
 
+
+
+// normal routes
 userRouter.route("/register").post(registrationsUser);
 userRouter.route("/activate-account").post(activateAccount);
+userRouter.route("/login").post(loginUser);
+userRouter.route("/refresh-token").get(updateAccessToken);
 
-export default userRouter;
+
+
+
+
+// authenticated routes
+userRouter.route("/logout").get(isAuthenticated, authorizedRole("admin", "user"), logoutUser);
+userRouter.route("/user").get(isAuthenticated, authorizedRole("admin", "user"), getUserById);
+userRouter.route("/update-userinfo").put(isAuthenticated, authorizedRole("admin", "user"), updateUserInfo);
+userRouter.route("/change-email").post(isAuthenticated, authorizedRole("admin", "user"), changeEmail);
+userRouter.route("/change-email").post(isAuthenticated, authorizedRole("admin", "user"), changeEmail);
+userRouter.route("/email-verification").put(isAuthenticated, authorizedRole("admin", "user"), emailVerification);
+
+
+
+
+
+
+export default userRouter;  
