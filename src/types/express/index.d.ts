135d198f51be81af7b504c38056a9d8e555851fb
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-07 12:40:19 +0530

    define types of files

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { IUserSchema } from "../userTypes";
+import { Multer } from 'multer';
 
 declare global {
   namespace Express {
     interface Request {
-      user?: Record<IUserSchema>
+      user?: Record<IUserSchema>;
+      file?: Multer.File;
+      files?: Multer.File[];
     }
   }
 }
