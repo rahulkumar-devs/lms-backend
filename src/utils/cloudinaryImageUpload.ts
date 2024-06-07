e6ac57a3e71cad48b64b4e1fcb8b0ea731bc86b2
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-07 12:34:37 +0530

    add cloudinary upload images

---
+++ src/utils/cloudinaryImageUpload.ts
@@ -0,0 +1,81 @@
+import { NextFunction, Request, Response } from "express";
+import cloudinary from "../configurations/cloudinary-connections";
+import fs from "fs";
+import path from "path";
+
+export interface IFileDetails {
+  filePath: string;
+  folder: string;
+  width?: number;
+  height?: number;
+}
+
+const uploadImageToCloudinary = async ({
+  filePath,
+  folder,
+  width = 300,
+  height = 300,
+}: IFileDetails) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder: folder,
+      quality_analysis: true,
+      width: width,
+      height: height,
+    });
+
+    const resultInfo = {
+      public_id: result.public_id,
+      url: result.url,
+    };
+
+    // Clean up the local file after upload
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+
+    return resultInfo;
+  } catch (error) {
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+
+    throw error;
+  }
+};
+
+export default uploadImageToCloudinary;
+
+
+// check file Already exist in cloudinary
+
+
+// Function to check if a file exists in Cloudinary
+export const checkFileExistsInCloudinary = async (publicId: string): Promise<boolean> => {
+  try {
+    // Perform a search in Cloudinary using the public ID
+    const searchResult = await cloudinary.search
+      .expression(`public_id:${publicId}`)
+      .execute();
+
+
+
+    // If any results are returned, the file exists in Cloudinary
+    return searchResult.total_count > 0;
+  } catch (error) {
+    console.error(`Error checking file existence in Cloudinary: ${publicId}`, error);
+    throw new Error("Failed to check file existence in Cloudinary");
+  }
+};
+
+export const deleteCloudinaryFile = async (publicId: string): Promise<boolean> => {
+  try {
+
+    return await cloudinary.uploader.destroy(publicId)
+
+  } catch (error) {
+    throw new Error("Failed to delete file existence in Cloudinary");
+
+  }
+
+}
