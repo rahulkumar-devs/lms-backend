a3d49927f3a09f0502cba37e2cc8e644c33936b8
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-08 15:05:02 +0530

    add answer reply

--- src/services/course.service.ts
+++ src/services/course.service.ts
@@ -33,7 +33,7 @@ export const addQuestions = expressAsyncHandler(
 
             courseContent.questions.push({ question, questionReplies: [], user: req?.user?._id } as any)
 
-            console.log(courseContent.questions)
+
             await course.save();
 
             res.status(201).json({ message: 'Question added successfully' });
@@ -42,3 +42,53 @@ export const addQuestions = expressAsyncHandler(
         }
     }
 );
+
+// <======Questions Answer========>
+// <======>
+
+interface IAddAnswer {
+    courseId: string;
+    contentId: string;
+    answer: string;
+    questionId: string;
+
+
+}
+
+export const addAnswer = expressAsyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+
+
+            const { courseId, contentId, answer, questionId } = req.body as IAddAnswer;
+
+            if (!isValidObjectId(contentId) || !isValidObjectId(contentId) || !isValidObjectId(questionId))
+                return next(createHttpError(400, ' Not a Valid ID '));
+
+            const course = await courseModel.findById(courseId);
+
+            const courseContent = course?.courseData.find((item) => String(item._id) === contentId); 
+            console.log(courseContent)
+            if (!courseContent) {
+                return next(createHttpError(400, 'Invalid content id'));
+            }
+
+            const question = courseContent.questions.find((item) => String(item._id) === questionId);
+
+            if (!question) {
+                return next(createHttpError(400, 'Invalid question id'));
+            }
+
+            question.questionReplies.push({ answer: answer } as any)
+
+
+            await course?.save()
+
+            res.status(201).json({ message: 'Question added successfully' });
+
+        } catch (error: any) {
+            return next(createHttpError(500, error.message));
+
+        }
+
+    })
