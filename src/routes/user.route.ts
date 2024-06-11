dd1946010644c2251d594bcbd6fd8803a5e1e4b8
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-11 09:08:14 +0530

    add user route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,8 +6,8 @@ import { loginUser, logoutUser } from "../controllers/auth/loginUser";
 import { isAuthenticated } from "../middleware/authentication";
 import authorizedRole from "../middleware/authorizedRole";
 import { updateAccessToken } from "../controllers/auth/updateAccessToken";
-import { changeEmail, deleteUser, emailVerification, getUserById, updateUserInfo } from "../services/user.service";
-import rolePermission, { socialAuth } from "../controllers/auth/rolePermission";
+import { changeEmail, deleteUser, emailVerification, getAllUsers, getUserById, updateUserInfo } from "../services/user.service";
+import rolePermission, { removeRolePermission, socialAuth } from "../controllers/auth/rolePermission";
 import { forgotPassword, verifyForgotPassword } from "../controllers/auth/forgotPassword";
 import { upload } from "../middleware/multerUploadFiles";
 
@@ -36,11 +36,14 @@ userRouter.route("/logout").get(isAuthenticated, authorizedRole("admin"), logout
 userRouter.route("/user").get(isAuthenticated, authorizedRole("admin", "user"), getUserById);
 userRouter.route("/update-userinfo").put(isAuthenticated, authorizedRole("admin", "user"), upload.single("avatar"), updateUserInfo);
 userRouter.route("/change-email").post(isAuthenticated, authorizedRole("admin", "user"), changeEmail);
-userRouter.route("/email-verification").patch(isAuthenticated, authorizedRole("admin", "user"), emailVerification);
-userRouter.route("delete-user").delete(isAuthenticated, authorizedRole("admin", "user"), deleteUser);
-userRouter.route("/role-permission").put(isAuthenticated, authorizedRole("admin"), rolePermission);
 
 
+// <=====Only Admin Accessable=====>
+userRouter.route("/email-verification").put(isAuthenticated, authorizedRole("admin", "user"), emailVerification);
+userRouter.route("/delete-user/:userId").delete(isAuthenticated, authorizedRole("admin"), deleteUser);
+userRouter.route("/role-permission/:userId").put(isAuthenticated, authorizedRole("admin"), rolePermission);
+userRouter.route("/remove-role-permission/:userId").put(isAuthenticated, authorizedRole("admin"), removeRolePermission);
+userRouter.route("/users").get(isAuthenticated, authorizedRole("admin"), getAllUsers);
 
 // aditional features
 
