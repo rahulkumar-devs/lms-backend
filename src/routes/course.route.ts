74c6bf2d779ccb15d3dc533bbc6d87975d8df0b7
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-11 09:08:49 +0530

    add course route

--- src/routes/course.route.ts
+++ src/routes/course.route.ts
@@ -1,10 +1,10 @@
 import express from "express"
-import { uploadCourse } from "../controllers/course/course";
+import { deleteCourse, uploadCourse } from "../controllers/course/course";
 import { isAuthenticated } from './../middleware/authentication';
 import authorizedRole from './../middleware/authorizedRole';
 import { upload } from "../middleware/multerUploadFiles";
 import { updateCourse } from "../controllers/course/updateCourse";
-import { getAllCourses, getCourseForElibigleUser, getSingleCourse } from "../controllers/course/getCourses";
+import { getAllCourses, getAllCoursesForAdmin, getCourseForElibigleUser, getSingleCourse } from "../controllers/course/getCourses";
 import { addAnswer, addQuestions, addReplyOnReview, addReview } from "../services/course.service";
 const courseRoutes = express.Router();
 
@@ -20,6 +20,8 @@ courseRoutes.route("/get-all-courses").get(getAllCourses)
 // <==========Protected routes================>
 // <==========================================>
 
+courseRoutes.route("/all-courses").put(isAuthenticated, authorizedRole("admin"), getAllCoursesForAdmin)
+
 courseRoutes.route("/upload-course").post(isAuthenticated, authorizedRole("admin"),
     upload.fields([{ name: 'courseThumbnail', maxCount: 1 }, { name: "videoThumbnail", maxCount: 1 }])
     ,
@@ -29,11 +31,19 @@ courseRoutes.route("/update-course/:courseId").put(isAuthenticated, authorizedRo
     ,
     updateCourse);
 
-courseRoutes.route("/get-course-content/:courseId").get(isAuthenticated, authorizedRole("user", "admin", "member"), getCourseForElibigleUser)
-courseRoutes.route("/add-question").put(isAuthenticated, addQuestions);
+
+courseRoutes.route("/delete-course/:courseId").delete(isAuthenticated, authorizedRole("admin"), deleteCourse)
+
+
+//<=====****************====>
+//<=====General  Routes====>
+//<=====****************====>
+courseRoutes.route("/get-course-content/:courseId").get(isAuthenticated, getCourseForElibigleUser)
+courseRoutes.route("/add-question").put(isAuthenticated, authorizedRole("user", "member"), addQuestions);
 courseRoutes.route("/add-answer").put(isAuthenticated, addAnswer);
 courseRoutes.route("/add-review/:courseId").put(isAuthenticated, addReview);
-courseRoutes.route("/add-review-reply").put(isAuthenticated,addReplyOnReview)
+courseRoutes.route("/add-review-reply").put(isAuthenticated, addReplyOnReview)
+
 
 
 
